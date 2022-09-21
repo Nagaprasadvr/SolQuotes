f6a703978b972c6a85510989184b39aaba368ccf
Nagaprasad V R <81755170+Nagaprasadvr@users.noreply.github.com>
2022-09-21 07:06:39 +0530

    ..

--- app/src/updateBlogAccount.ts
+++ app/src/updateBlogAccount.ts
@@ -12,9 +12,6 @@ let blgContent = "The faster you move through space , the slower you travel thro
 const ProgramId =new anchor.web3.PublicKey("A1Z5g145cwSqtHv5aEF7MeARfT46cm8cq8w33inoisuA")
 // const provider = new anchor.An
 const idl = JSON.parse(fs.readFileSync("./target/idl/solblog.json","utf8"));
-//const Keypair:anchor.web3.Keypair = anchor.web3.Keypair.fromSecretKey(Uint8Array.from([125,215,2,14,10,163,144,235,149,163,195,26,108,51,188,87,198,74,23,110,157,186,167,176,239,61,212,204,153,146,147,218]))
-// const Keypair = anchor.web3.Keypair.generate();
-const Keypair = anchor.web3.Keypair.fromSecretKey(Uint8Array.from([125,215,2,14,10,163,144,235,149,163,195,26,108,51,188,87,198,74,23,110,157,186,167,176,239,61,212,204,153,146,147,218,85,229,221,209,250,178,94,134,72,232,111,154,201,194,118,208,26,68,236,118,74,202,225,52,111,178,199,191,45,11,102,57]))
 console.log(Keypair.publicKey.toBase58());
 const wallet = new anchor.Wallet(Keypair);
 const con = new anchor.web3.Connection("https://api.devnet.solana.com");
